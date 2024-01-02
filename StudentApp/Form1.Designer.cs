e78e2d2e11297f8fc98a3baf8f26509ce17eecc5
anass-gouzgar <go.anass.gouzgar@gmail.com>
2024-01-02 18:45:23 +0100

    just testing

--- StudentApp/Form1.Designer.cs
+++ StudentApp/Form1.Designer.cs
@@ -36,26 +36,24 @@
             this.ButtonUpdate = new System.Windows.Forms.Button();
             this.ButtonDelete = new System.Windows.Forms.Button();
             this.ButtonReset = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.txtStudentAdresse = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.txtStudentFname = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.txtStudentNumber = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.StudentDataGrid)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // ButtonAdd
             // 
             this.ButtonAdd.BackColor = System.Drawing.Color.DodgerBlue;
             this.ButtonAdd.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ButtonAdd.Location = new System.Drawing.Point(49, 219);
+            this.ButtonAdd.Location = new System.Drawing.Point(37, 178);
+            this.ButtonAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ButtonAdd.Name = "ButtonAdd";
-            this.ButtonAdd.Size = new System.Drawing.Size(98, 45);
+            this.ButtonAdd.Size = new System.Drawing.Size(74, 37);
             this.ButtonAdd.TabIndex = 0;
             this.ButtonAdd.Text = "Add";
             this.ButtonAdd.UseVisualStyleBackColor = false;
@@ -65,13 +63,14 @@
             // 
             this.StudentDataGrid.AllowUserToOrderColumns = true;
             this.StudentDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.StudentDataGrid.Location = new System.Drawing.Point(99, 326);
+            this.StudentDataGrid.Location = new System.Drawing.Point(75, 277);
+            this.StudentDataGrid.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.StudentDataGrid.Name = "StudentDataGrid";
             this.StudentDataGrid.ReadOnly = true;
             this.StudentDataGrid.RowHeadersWidth = 51;
             this.StudentDataGrid.RowTemplate.Height = 24;
             this.StudentDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.StudentDataGrid.Size = new System.Drawing.Size(740, 157);
+            this.StudentDataGrid.Size = new System.Drawing.Size(555, 128);
             this.StudentDataGrid.TabIndex = 1;
             this.StudentDataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.StudentDataGrid_CellContentClick);
             this.StudentDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.StudentDataGrid_CellContentClick);
@@ -80,27 +79,30 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(46, 59);
+            this.label1.Location = new System.Drawing.Point(103, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 25);
+            this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "S. Name :";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtStudentName
             // 
-            this.txtStudentName.Location = new System.Drawing.Point(151, 63);
+            this.txtStudentName.Location = new System.Drawing.Point(202, 50);
+            this.txtStudentName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtStudentName.Name = "txtStudentName";
-            this.txtStudentName.Size = new System.Drawing.Size(100, 22);
+            this.txtStudentName.Size = new System.Drawing.Size(76, 20);
             this.txtStudentName.TabIndex = 3;
             // 
             // ButtonUpdate
             // 
             this.ButtonUpdate.BackColor = System.Drawing.Color.Lime;
             this.ButtonUpdate.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ButtonUpdate.Location = new System.Drawing.Point(244, 219);
+            this.ButtonUpdate.Location = new System.Drawing.Point(183, 178);
+            this.ButtonUpdate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ButtonUpdate.Name = "ButtonUpdate";
-            this.ButtonUpdate.Size = new System.Drawing.Size(98, 45);
+            this.ButtonUpdate.Size = new System.Drawing.Size(74, 37);
             this.ButtonUpdate.TabIndex = 0;
             this.ButtonUpdate.Text = "Update";
             this.ButtonUpdate.UseVisualStyleBackColor = false;
@@ -110,9 +112,10 @@
             // 
             this.ButtonDelete.BackColor = System.Drawing.Color.Red;
             this.ButtonDelete.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ButtonDelete.Location = new System.Drawing.Point(439, 219);
+            this.ButtonDelete.Location = new System.Drawing.Point(329, 178);
+            this.ButtonDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ButtonDelete.Name = "ButtonDelete";
-            this.ButtonDelete.Size = new System.Drawing.Size(98, 45);
+            this.ButtonDelete.Size = new System.Drawing.Size(74, 37);
             this.ButtonDelete.TabIndex = 0;
             this.ButtonDelete.Text = "Delete";
             this.ButtonDelete.UseVisualStyleBackColor = false;
@@ -122,39 +125,31 @@
             // 
             this.ButtonReset.BackColor = System.Drawing.Color.Yellow;
             this.ButtonReset.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ButtonReset.Location = new System.Drawing.Point(634, 219);
+            this.ButtonReset.Location = new System.Drawing.Point(476, 178);
+            this.ButtonReset.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ButtonReset.Name = "ButtonReset";
-            this.ButtonReset.Size = new System.Drawing.Size(98, 45);
+            this.ButtonReset.Size = new System.Drawing.Size(74, 37);
             this.ButtonReset.TabIndex = 0;
             this.ButtonReset.Text = "Reset";
             this.ButtonReset.UseVisualStyleBackColor = false;
             this.ButtonReset.Click += new System.EventHandler(this.ButtonReset_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(58, 123);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 25);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label1";
-            this.label2.Click += new System.EventHandler(this.label1_Click);
-            // 
             // txtStudentAdresse
             // 
-            this.txtStudentAdresse.Location = new System.Drawing.Point(151, 126);
+            this.txtStudentAdresse.Location = new System.Drawing.Point(202, 100);
+            this.txtStudentAdresse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtStudentAdresse.Name = "txtStudentAdresse";
-            this.txtStudentAdresse.Size = new System.Drawing.Size(100, 22);
+            this.txtStudentAdresse.Size = new System.Drawing.Size(76, 20);
             this.txtStudentAdresse.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(46, 123);
+            this.label3.Location = new System.Drawing.Point(103, 100);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(96, 25);
+            this.label3.Size = new System.Drawing.Size(76, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Adresse :";
             this.label3.Click += new System.EventHandler(this.label1_Click);
@@ -163,49 +158,41 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(330, 59);
+            this.label4.Location = new System.Drawing.Point(356, 48);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(97, 25);
+            this.label4.Size = new System.Drawing.Size(77, 20);
             this.label4.TabIndex = 2;
             this.label4.Text = "F. Name :";
             this.label4.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtStudentFname
             // 
-            this.txtStudentFname.Location = new System.Drawing.Point(457, 63);
+            this.txtStudentFname.Location = new System.Drawing.Point(460, 51);
+            this.txtStudentFname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtStudentFname.Name = "txtStudentFname";
-            this.txtStudentFname.Size = new System.Drawing.Size(119, 22);
+            this.txtStudentFname.Size = new System.Drawing.Size(90, 20);
             this.txtStudentFname.TabIndex = 3;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(330, 123);
+            this.label5.Location = new System.Drawing.Point(329, 100);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(132, 25);
+            this.label5.Size = new System.Drawing.Size(104, 20);
             this.label5.TabIndex = 2;
             this.label5.Text = "Mobile Num : ";
             this.label5.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtStudentNumber
             // 
-            this.txtStudentNumber.Location = new System.Drawing.Point(459, 127);
+            this.txtStudentNumber.Location = new System.Drawing.Point(460, 102);
+            this.txtStudentNumber.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtStudentNumber.Name = "txtStudentNumber";
-            this.txtStudentNumber.Size = new System.Drawing.Size(117, 22);
+            this.txtStudentNumber.Size = new System.Drawing.Size(89, 20);
             this.txtStudentNumber.TabIndex = 3;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(595, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(313, 122);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button1
             // 
@@ -213,9 +200,10 @@
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(819, 219);
+            this.button1.Location = new System.Drawing.Point(614, 178);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 45);
+            this.button1.Size = new System.Drawing.Size(67, 37);
             this.button1.TabIndex = 5;
             this.button1.Text = "EXIT";
             this.button1.UseVisualStyleBackColor = false;
@@ -223,19 +211,17 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(935, 530);
+            this.ClientSize = new System.Drawing.Size(701, 431);
             this.ControlBox = false;
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtStudentNumber);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtStudentAdresse);
             this.Controls.Add(this.txtStudentFname);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtStudentName);
             this.Controls.Add(this.label1);
@@ -246,6 +232,7 @@
             this.Controls.Add(this.ButtonAdd);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -253,7 +240,6 @@
             this.Text = " Student Informations";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.StudentDataGrid)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -268,14 +254,12 @@
         private System.Windows.Forms.Button ButtonUpdate;
         private System.Windows.Forms.Button ButtonDelete;
         private System.Windows.Forms.Button ButtonReset;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtStudentAdresse;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtStudentFname;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtStudentNumber;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
     }
 }
